1e44db5629ca41a7ccb722596120743f14a33619
Kevin Brena <kevin.brena99@gmail.com>
2026-08-14 07:56:38 -0500

    configurar con riverpod el llamado al listado de peliculas

---
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -0,0 +1,32 @@
+
+
+import 'package:cinemapedia/domain/entiites/movie_entity.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/legacy.dart';
+
+final nowPlayingMoviesProvider = StateNotifierProvider<MoviesNotifier,List<MovieEntity>>((ref){
+
+  final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPlaying;
+
+  return MoviesNotifier(fetchMoreMovies: fetchMoreMovies);
+});
+
+typedef MovieCallback = Future<List<MovieEntity>> Function({int page});
+
+
+class MoviesNotifier extends StateNotifier<List<MovieEntity>>{
+
+  int currentPage = 0;
+  MovieCallback fetchMoreMovies;
+
+  MoviesNotifier({
+    required this.fetchMoreMovies
+  }): super ([]);
+
+  Future<void> loadNextPage() async{
+    currentPage++;
+    final List<MovieEntity> movies = await fetchMoreMovies(page: currentPage);
+    state = [...state,...movies];
+  }
+  
+}
---
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -0,0 +1,10 @@
+
+
+import 'package:cinemapedia/infrastructure/datasources/moviedb_datasource_impl.dart';
+import 'package:cinemapedia/infrastructure/repositories/movie_repository_impl.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final movieRepositoryProvider = Provider((ref) {
+
+  return MovieRepositoryImpl(MoviedbDatasourceImpl());
+});
